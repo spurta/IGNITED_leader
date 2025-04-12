a64217dd1dbe35c9a3f246877b5d2d2363b3221c
spurta <spurtapurton@gmail.com>
2025-04-13 00:48:06 +1000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/pdf', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: "new",
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
