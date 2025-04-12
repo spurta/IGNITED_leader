a327311214b8a6c92bd846228cfdf9d0218d9ac7
spurta <spurtapurton@gmail.com>
2025-04-13 01:36:18 +1000

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/pdf', async (req, res) => {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
+      executablePath: await chromium.executablePath || '/usr/bin/chromium-browser',
       headless: chromium.headless
     });
 
