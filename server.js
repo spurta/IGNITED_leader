fad21becf4277c0bf26386f64f5c24e0847ec45c
spurta <spurtapurton@gmail.com>
2025-04-13 00:05:06 +1000

    Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ app.get('/pdf', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless
-    });
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath || '/usr/bin/google-chrome-stable',
+  headless: chromium.headless
+});
+
 
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 1600 });
